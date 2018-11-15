e933305d78afb31b8eacb6a140831f5b6d7332f6
Sophoana Pan <span@Sophoanas-MacBook-Pro.local>
2018-11-14 21:51:31 -0500

    MongoDb and Authentication

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Middleware
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
 
-//app.use(morgan('tiny));
-app.use(morgan('combined'));
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    // res.send('Hello from my library app');
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-})
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }
+];
 
-app.listen(3300, function(){
-    debug('listening at port ' + chalk.green('3300'));
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
+
+app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
